e1f613a62eb44773aae3650e6ba42affad6d4fb7
Unknown <juanan.pujals@gmail.com>
2018-04-05 09:30:04 +0200

    Prueba Mediaqueries (clases)

--- js/triviaWow.js
+++ js/triviaWow.js
@@ -1,29 +1,31 @@
+window.onload = function(){
 
+};
 
-function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        myFunction(xmlhttp);
-      }
-    }
-    xmlhttp.open("GET", "triviaWow.xml", true);
-    xmlhttp.send();
-  }
+// function loadXMLDoc() {
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//         myFunction(xmlhttp);
+//       }
+//     }
+//     xmlhttp.open("GET", "triviaWow.xml", true);
+//     xmlhttp.send();
+//   }
 
-  function myFunction(xml) {
-    var i;
-    var xmlDoc = xml.responseXML;
-    console.log(xmlDoc);
-    var enunciado = "";
-    var form = "";
-    var x = xmlDoc.getElementsByTagName("pregunta");
-    for (i = 0; i <x.length; i++) { 
-      enunciado += "<h3>"+x[i].getElementsByTagName("Enunciado").nodeValue+"</h3><br/>";
-      /*form += '<input type="radio" name="pregunta" value="'+i+'" checked><label>' +
-      x[i].getElementsByTagName("Repuestas")[i].childNodes[0].nodeValue +
-      "</label>";*/
-    }
-    document.getElementById("demo").innerHTML = enunciado;
-}
+//   function myFunction(xml) {
+//     var i;
+//     var xmlDoc = xml.responseXML;
+//     console.log(xmlDoc);
+//     var enunciado = "";
+//     var form = "";
+//     var x = xmlDoc.getElementsByTagName("pregunta");
+//     for (i = 0; i <x.length; i++) { 
+//       enunciado += "<h3>"+x[i].getElementsByTagName("Enunciado").nodeValue+"</h3><br/>";
+//       /*form += '<input type="radio" name="pregunta" value="'+i+'" checked><label>' +
+//       x[i].getElementsByTagName("Repuestas")[i].childNodes[0].nodeValue +
+//       "</label>";*/
+//     }
+//     document.getElementById("demo").innerHTML = enunciado;
+// }
 
