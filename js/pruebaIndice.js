88c30825ad26ae4bd71822cb8b88158cc2b4d9fb
Juan Antonio Pujals Echevarría <juanan.pujals@gmail.com>
2018-04-06 13:10:29 +0200

    añadiendo estilos media queries

--- js/pruebaIndice.js
+++ js/pruebaIndice.js
@@ -3,6 +3,13 @@ window.onload = function() {
         $( "#accordion" ).accordion();
       } );
       cargarPreguntasXML();
+
+    $("#left").on("click",function(){
+        closeSideBar();
+    })
+    $("#right").on("click",function(){
+        openSideBar();
+    })
     };
     
     
@@ -12,6 +19,7 @@ window.onload = function() {
             url: "https://rawgit.com/Juanan313/Formulario-Autocorrecion/master/js/triviaWow.xml",
             contentType: "application/json; charset=utf-8",
             dataType: "xml",
+            
             success: function (data) {
                 PREGUNTASXML = data;
     
@@ -21,4 +29,18 @@ window.onload = function() {
                 alert("Error al procesar la petición AJAX de Preguntas.");
             }
         }); 
+    }
+
+    function closeSideBar() {
+        element = document.getElementById("xsocial");
+        element.style.display = "none";
+        button = document.getElementById("right");
+        button.style.display = "block";
+    }
+    
+    function openSideBar() {
+        element = document.getElementById("xsocial");
+        element.style.display = "block";
+        button = document.getElementById("right");
+        button.style.display = "none";
     }
