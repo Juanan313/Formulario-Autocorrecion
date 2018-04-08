5c79a3c591778812e883e16a2b87469c08af64d0
juanan pujals echavarria <juanan.pujals@gmail.com>
2018-04-08 21:31:21 +0200

    Cargan Preguntas formateadas

--- js/pruebaIndice.js
+++ js/pruebaIndice.js
@@ -1,4 +1,4 @@
-var PREGUNTASXML = [];
+var PREGUNTASXML;
 
 /*--- Función onload para cargar todo los eventos y las llamadas a las funciones -----*/
 window.onload = function () {
@@ -6,11 +6,13 @@ window.onload = function () {
         $("#accordion").accordion();
     });
     cargarPreguntasXML();
-    prepararPreguntasXML();
 
-    $("#loadXml").on("clikc", function () {
-        crearRadio(1);
-    });
+
+
+
+    // $("#loadXml").on("clikc", function () {
+    //     crearRadio(1);
+    // });
     $("#left").on("click", function () {
         closeSideBar();
     });
@@ -18,81 +20,111 @@ window.onload = function () {
         openSideBar();
     });
 };
-    
-    function cargarPreguntasXML (){
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                parser = new DOMParser();
-	            xmlDoc = parser.parseFromString(xhttp.responseText,"text/xml");
-                PREGUNTASXML = $(xmlDoc).find("pregunta");
-            }
-        };
-        xhttp.open("GET", "https://raw.githubusercontent.com/Juanan313/Formulario-Autocorrecion/master/js/triviaWow.xml", true);
-        xhttp.send();
-        
-        
-    }
 
-    /* ----- Funcion tratar XML -----------*/
+function cargarPreguntasXML() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            parser = new DOMParser();
+            xmlDoc = parser.parseFromString(xhttp.responseText, "text/xml");
+            PREGUNTASXML = xmlDoc.getElementsByTagName("pregunta");
+            prepararPreguntasXML()
+        }
+    };
+    xhttp.open("GET", "https://raw.githubusercontent.com/Juanan313/Formulario-Autocorrecion/master/js/triviaWow.xml", true);
+    xhttp.send();
 
-    function prepararPreguntasXML() {
-        var numPreguntas = PREGUNTASXML.length;
-        for (var i = 0; i < numPreguntas; i++) {
+
+}
+
+/* ----- Funcion tratar XML -----------*/
+
+function prepararPreguntasXML() {
+    var numPreguntas = PREGUNTASXML.length;
+    for (var i = 0; i < numPreguntas; i++) {
 
         var tipo = PREGUNTASXML[i].getElementsByTagName('Tipo')[0].innerHTML;
 
         switch (tipo) {
-            case "Selección":
+            case " Selección ":
                 crearRadio(i);
                 break;
-            case "Múltiple":
+            case " Múltiple ":
                 crearCheck(i);
                 break;
             case "selecttext":
                 crearText(i);
             default:
                 console.log("default");
-            }
-            console.log("------");
         }
+        console.log("------");
     }
-
-    
-    function crearRadio(indice) {
-        var pregunta = $(PREGUNTASXML.get(indice));
-        var enunciado = $("<h3/>").html(pregunta.find('Enunciado').text());
-        var accordionPregunta = $("<div/>").html(pregunta.find())
-        return enunciado;
-    }
-
-    function crearCheck(indice) {
-        
-    }
-
-    function crearText(indice) {
-        
-    }
+}
 
 
+function crearRadio(indice) {
+    var pregunta = PREGUNTASXML[indice];
+    var texto;
+    texto = $(pregunta).find("Enunciado").text();
+
+    var enunciado = $("<h3/>").append("texto");
+    var accordionPregunta = $("<div/>").html("Elija una posible respuesta: ")
+    var respuestas = "<form id='" + indice + "'>";
+    var textoRespuestas = $(pregunta).find("Respuestas");
+    respuestas += "<input type='radio' name='pregunta' value='a' checked> <label>" + textoRespuestas.find("A").text() + "</label><br/>";
+    respuestas += "<input type='radio' name='pregunta' value='b' > <label>" + textoRespuestas.find("B").text() + "</label><br/>";
+    respuestas += "<input type='radio' name='pregunta' value='c' > <label>" + textoRespuestas.find("C").text() + "</label><br/>";
+    respuestas += "<input type='radio' name='pregunta' value='d' > <label>" + textoRespuestas.find("D").text() + "</label><br/>";
+    respuestas += "</form>";
+    accordionPregunta.append(respuestas);
+
+    $("#accordion").append("<h3>" + texto + "</h3>");
+    $("#accordion").append(accordionPregunta);
+}
+
+function crearCheck(indice) {
+    var pregunta = PREGUNTASXML[indice];
+    var texto;
+    texto = $(pregunta).find("Enunciado").text();
+
+    var enunciado = $("<h3 style{background-color: '93db5c81'}></h3>").append("texto");
+    var accordionPregunta = $("<div/>").html("Elija todas las posibles respuestas que considere: ")
+    var respuestas = "<form id='" + indice + "'>";
+    var textoRespuestas = $(pregunta).find("Respuestas");
+    respuestas += "<input type='checkbox' name='pregunta' value='a' > <label>" + textoRespuestas.find("A").text() + "</label><br/>";
+    respuestas += "<input type='checkbox' name='pregunta' value='b' > <label>" + textoRespuestas.find("B").text() + "</label><br/>";
+    respuestas += "<input type='checkbox' name='pregunta' value='c' > <label>" + textoRespuestas.find("C").text() + "</label><br/>";
+    respuestas += "<input type='checkbox' name='pregunta' value='d' > <label>" + textoRespuestas.find("D").text() + "</label><br/>";
+    respuestas += "</form>";
+    accordionPregunta.append(respuestas);
+
+    $("#accordion").append("<h3>" + texto + "</h3>");
+    $("#accordion").append(accordionPregunta);
+}
+
+function crearText(indice) {
+    console.log("todavia no esta lista");
+}
 
 
 
 
 
 
-    /*--- Abre y cierra la barra lateral ----*/
-    function closeSideBar() {
-        element = document.getElementById("xsocial");
-        element.style.display = "none";
-        button = document.getElementById("right");
-        button.style.display = "block";
-    }
-    
-    function openSideBar() {
-        element = document.getElementById("xsocial");
-        element.style.display = "block";
-        button = document.getElementById("right");
-        button.style.display = "none";
-    }
+
+
+/*--- Abre y cierra la barra lateral ----*/
+function closeSideBar() {
+    element = document.getElementById("xsocial");
+    element.style.display = "none";
+    button = document.getElementById("right");
+    button.style.display = "block";
+}
+
+function openSideBar() {
+    element = document.getElementById("xsocial");
+    element.style.display = "block";
+    button = document.getElementById("right");
+    button.style.display = "none";
+}
     /* --------------------------------------*/
