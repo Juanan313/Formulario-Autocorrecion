c67637da4877775ec2077d6b45229e09fc1fcbfd
Unknown <juanan.pujals@gmail.com>
2018-04-08 17:02:29 +0200

    carga XML + font style

--- js/pruebaIndice.js
+++ js/pruebaIndice.js
@@ -1,36 +1,87 @@
-window.onload = function() {
-    $( function() {
-        $( "#accordion" ).accordion();
-      } );
-      cargarPreguntasXML();
+var PREGUNTASXML = [];
 
-    $("#left").on("click",function(){
+/*--- Función onload para cargar todo los eventos y las llamadas a las funciones -----*/
+window.onload = function () {
+    $(function () {
+        $("#accordion").accordion();
+    });
+    cargarPreguntasXML();
+    prepararPreguntasXML();
+
+    $("#loadXml").on("clikc", function () {
+        crearRadio(1);
+    });
+    $("#left").on("click", function () {
         closeSideBar();
-    })
-    $("#right").on("click",function(){
+    });
+    $("#right").on("click", function () {
         openSideBar();
-    })
-    };
+    });
+};
     
-    
-    function cargarPreguntasXML() {
-        $.ajax({
-            type: "GET",
-            url: "https://rawgit.com/Juanan313/Formulario-Autocorrecion/master/js/triviaWow.xml",
-            contentType: "application/json; charset=utf-8",
-            dataType: "xml",
-            
-            success: function (data) {
-                PREGUNTASXML = data;
-    
-            },
-            error: function (e) {
-                console.log(e.responseText);
-                alert("Error al procesar la petición AJAX de Preguntas.");
+    function cargarPreguntasXML (){
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                parser = new DOMParser();
+	            xmlDoc = parser.parseFromString(xhttp.responseText,"text/xml");
+                PREGUNTASXML = $(xmlDoc).find("pregunta");
             }
-        }); 
+        };
+        xhttp.open("GET", "https://raw.githubusercontent.com/Juanan313/Formulario-Autocorrecion/master/js/triviaWow.xml", true);
+        xhttp.send();
+        
+        
     }
 
+    /* ----- Funcion tratar XML -----------*/
+
+    function prepararPreguntasXML() {
+        var numPreguntas = PREGUNTASXML.length;
+        for (var i = 0; i < numPreguntas; i++) {
+
+        var tipo = PREGUNTASXML[i].getElementsByTagName('Tipo')[0].innerHTML;
+
+        switch (tipo) {
+            case "Selección":
+                crearRadio(i);
+                break;
+            case "Múltiple":
+                crearCheck(i);
+                break;
+            case "selecttext":
+                crearText(i);
+            default:
+                console.log("default");
+            }
+            console.log("------");
+        }
+    }
+
+    
+    function crearRadio(indice) {
+        var pregunta = $(PREGUNTASXML.get(indice));
+        var enunciado = $("<h3/>").html(pregunta.find('Enunciado').text());
+        var accordionPregunta = $("<div/>").html(pregunta.find())
+        return enunciado;
+    }
+
+    function crearCheck(indice) {
+        
+    }
+
+    function crearText(indice) {
+        
+    }
+
+
+
+
+
+
+
+
+    /*--- Abre y cierra la barra lateral ----*/
     function closeSideBar() {
         element = document.getElementById("xsocial");
         element.style.display = "none";
@@ -44,3 +95,4 @@ window.onload = function() {
         button = document.getElementById("right");
         button.style.display = "none";
     }
+    /* --------------------------------------*/
